bfa2e663a6a2ca169729a789440f9fcb73584b3e
EthanOickle <147327174+OickleEthan@users.noreply.github.com>
2023-10-09 20:04:03 -0400

    changed box dynamics

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // JavaScript code to generate boxes dynamically
 const boxContainer = document.querySelector('.box-container');
+let boxesPerColumn = 2; // Initial number of boxes per column
 
 // Number of boxes per row (based on screen width)
 const boxesPerRow = Math.floor(window.innerWidth / 100); // Assuming each box is 100px wide
-
-// Number of boxes per column
-const boxesPerColumn = 2;
-
-// Total number of boxes
-const totalBoxes = boxesPerRow * boxesPerRow;
+const totalBoxes = boxesPerRow * boxesPerColumn; // Total number of boxes
 
 // Generate boxes dynamically
 for (let i = 1; i <= totalBoxes; i++) {
@@ -17,3 +13,23 @@ for (let i = 1; i <= totalBoxes; i++) {
     box.innerText = `Box ${i}`;
     boxContainer.appendChild(box);
 }
+
+// Function to generate additional boxes when "More Results" button is clicked
+document.getElementById('moreResultsBtn').addEventListener('click', function() {
+    // Increase boxesPerColumn by 1
+    boxesPerColumn++;
+
+    // Remove existing boxes
+    boxContainer.innerHTML = '';
+
+    // Calculate new total number of boxes
+    const newTotalBoxes = boxesPerRow * boxesPerColumn;
+
+    // Generate boxes dynamically based on updated boxesPerColumn
+    for (let i = 1; i <= newTotalBoxes; i++) {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.innerText = `Box ${i}`;
+        boxContainer.appendChild(box);
+    }
+});
